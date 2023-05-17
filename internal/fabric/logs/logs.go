627e25e66c55cb0fa5252aebe9ca260a93cab613
TheDonrad <deseeagle@gmail.com>
2023-05-17 20:13:09 +0300

    iter20

--- internal/fabric/logs/logs.go
+++ internal/fabric/logs/logs.go
@@ -3,19 +3,14 @@ package logs
 import (
 	"log"
 	"os"
-	"sync"
 )
 
 var (
 	logger *log.Logger
-	once   *sync.Once
 )
 
 // Logger возвращает объект для логирования
 func Logger() *log.Logger {
-	once.Do(func() {
-		logger = log.New(os.Stdout, "error: ", log.Lshortfile)
-	})
-
+	logger = log.New(os.Stdout, "error: ", log.Lshortfile)
 	return logger
 }
